2bac44fb4f22b8ab44563b5180804931faeb9795
Rob <rob.dickerson.dev@gmail.com>
2017-12-19 11:25:30 -0800

    Refactor: chapter 4 closure
    made a fun little thing that calculates ems
    
    end_of_line

--- js-essentials/site/script.js
+++ js-essentials/site/script.js
@@ -1,18 +1,19 @@
-function Course(title, instructor, level, published, views) {
-    this.title = title;
-    this.instructor = instructor;
-    this.level = level;
-    this.published = published;
-    this.views = views;
-    this.updateViews = function () {
-        return ++this.views;
-    };
+function giveMeEms(pixels) {
+    var baseValue = 16;
+
+    function doTheMath() {
+        return pixels/baseValue;
+    }
+
+    return doTheMath;
 }
 
-var courses = [
-        new Course('Javascript Esssential', 'Morty Mc Mort Mort', 1, true, 0),
-        new Course('ES6', 'Mort Hendermort', 1, true, 123456)
-    ];
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var xLargeSize = giveMeEms(32);
 
-
-console.log(courses[1].updateViews());
+console.log('Small size: ', smallSize());
+console.log('Medium size: ', mediumSize());
+console.log('Large size: ', largeSize());
+console.log('Extra Large size: ', xLargeSize());
