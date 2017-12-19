cb894541a6241a0a409720cdcb1b745859bb283f
Rob <rob.dickerson.dev@gmail.com>
2017-12-19 10:36:52 -0800

    Refactor: chapter 4.6 iif
    end_of_line

--- js-essentials/site/script.js
+++ js-essentials/site/script.js
@@ -1,8 +1,10 @@
+var firstFraction = 7/9;
+var secondFraction = 13/24;
 
-var theBiggest = function (a, b) {
+var theBiggest = (function (a, b) {
     var result;
     a>b ? result = ['a', a] : result = ['b', b];
     return result;
-};
+})(firstFraction, secondFraction);
 
-console.log(theBiggest(7/9, 13/25));
+console.log(theBiggest);
