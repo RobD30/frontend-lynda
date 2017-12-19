db489d0967c6c6159df8d219c96861a82c1958d2
Rob <rob.dickerson.dev@gmail.com>
2017-12-19 11:12:47 -0800

    Refactor: chapter 4 object frames
    end_of_line

--- js-essentials/site/script.js
+++ js-essentials/site/script.js
@@ -1,14 +1,18 @@
-var course = new Object();
+function Course(title, instructor, level, published, views) {
+    this.title = title;
+    this.instructor = instructor;
+    this.level = level;
+    this.published = published;
+    this.views = views;
+    this.updateViews = function () {
+        return ++this.views;
+    };
+}
 
-var course = {
-    title: 'Javascript Essentials',
-    instructor: 'Morty Mc Mort Mort',
-    level: 1,
-    published: true,
-    views: 1,
-    updateViews: function () {
-        return ++course.views;
-    }
-};
+var courses = [
+        new Course('Javascript Esssential', 'Morty Mc Mort Mort', 1, true, 0),
+        new Course('ES6', 'Mort Hendermort', 1, true, 123456)
+    ];
 
-console.log(course.title, course.instructor);
+
+console.log(courses[1].updateViews());
