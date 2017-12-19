3fb9e18d310ad1d421316ad4b109364ee9daf562
Rob <rob.dickerson.dev@gmail.com>
2017-12-19 10:12:40 -0800

    Refactor: chapter 4.2 fun with fractions
    end_of_line

--- js-essentials/site/script.js
+++ js-essentials/site/script.js
@@ -1,21 +1,9 @@
-// Regular function, called explicitly by name:
-function multiply() {
-    var result = 3 * 4;
-    console.log("3 multiplied by 4 is ", result);
+function findBiggestFraction() {
+    a>b ? console.log('a: ', a) : console.log('b: ', b);
 }
-multiply();
 
-// Anonymous function stored in variable.
-// Invoked by calling the variable as a function:
-var divided = function() {
-    var result = 3 / 4;
-    console.log("3 divided by 4 is ", result);
-}
-divided();
 
-// Immediately Invoked Function Expression.
-// Runs as soon as the browser finds it:
-(function() {
-    var result = 12 / 0.75;
-    console.log("12 divided by 0.75 is ", result);
-}())
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
