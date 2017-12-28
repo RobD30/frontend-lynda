f0aa8af77bf570dae9894d51a5812f6b5aca99b3
Rob <rob.dickerson.dev@gmail.com>
2017-12-28 12:49:46 -0800

    Refactor: chapter 4 End This is cool
    end_of_line

--- js-essentials/site/script.js
+++ js-essentials/site/script.js
@@ -1,18 +1,19 @@
-function Course(title, instructor, level, published, views) {
-    this.title = title;
-    this.instructor = instructor;
-    this.level = level;
-    this.published = published;
-    this.views = views;
-    this.updateViews = function () {
-        return ++this.views;
-    };
-};
+function giveMeEms(pixels) {
+    var baseVal = 16;
 
-var courses = [
-    new Course('JS Essentials', 'Morty McMort Mort', 1, true, 0),
-    new Course('Geeb', 'Goblin', 1, true, 0),
-];
+    function doTheMath() {
+        return pixels/baseVal;
+    }
 
+    return doTheMath;
+}
 
-console.log(courses[1].updateViews());
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var xlargeSize = giveMeEms(32);
+
+console.log('Small size: ', smallSize());
+console.log('Medium size: ', mediumSize());
+console.log('Large size: ', largeSize());
+console.log('X-Large size: ', xlargeSize());
